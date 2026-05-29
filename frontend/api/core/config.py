9d45ef136b034de68e36a274f5fe4e58515afd4e
sudhagitam <sudhakarkatti@gmail.com>
2026-05-28 22:12:08 -0400

    Increase MAX_TEXT_CHARS limit to 90,000

--- frontend/api/core/config.py
+++ frontend/api/core/config.py
@@ -3,7 +3,7 @@ import os
 
 class Settings:
     MAX_UPLOAD_BYTES: int = int(os.environ.get("MAX_UPLOAD_BYTES", 20 * 1024 * 1024))
-    MAX_TEXT_CHARS: int   = int(os.environ.get("MAX_TEXT_CHARS", 50_000))
+    MAX_TEXT_CHARS: int   = int(os.environ.get("MAX_TEXT_CHARS", 90_000))
     TMP_DIR: str          = os.environ.get("TMP_DIR", "/tmp/docvoice")
     DEFAULT_LANG: str     = os.environ.get("DEFAULT_LANG", "en")
 
